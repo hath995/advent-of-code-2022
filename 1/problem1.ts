63d8e98dafb4ab05d24e1ebc21339b975392d0ad
Aaron Elligsen <aaron@screenmeet.com>
2022-12-01 19:07:33 +0100

    refactor a bit

--- 1/problem1.ts
+++ 1/problem1.ts
@@ -1,27 +1,29 @@
 import { assert } from 'console';
 import fs from 'fs';
 
+function parseInput(s: string): number[][] {
+    return s.split("\n\n").filter(x => x.length !== 0).map(x => x.split("\n").map(x => parseInt(x)))
+}
+
 const dataString = fs.readFileSync("./input.txt",{encoding:"utf8"});
-const data = dataString.split("\n\n").filter(x => x.length !== 0);
+const data = parseInput(dataString)
 
 const exampleString = fs.readFileSync("./example.txt",{encoding:"utf8"});
-const exampleData = exampleString.split("\n\n").filter(x => x.length !== 0);
+const exampleData = parseInput(exampleString);
 
-function part1(data: string[]): number {
-    let result = data.map(x => x.split("\n").map(x => parseInt(x)))
-    let totals = result.flatMap(items => items.reduce((x,y) => x+y,0));
+function part1(data: number[][]): number {
+    let totals = data.map(items => items.reduce((x,y) => x+y,0));
     return totals.reduce((best, current) => Math.max(best, current));
 }
 
-// let exampleResult = part1(exampleData);
-// assert(exampleResult === 24000, "should equal the sum of the elf with the most calories")
-// console.log(exampleResult);
+let exampleResult = part1(exampleData);
+assert(exampleResult === 24000, "should equal the sum of the elf with the most calories")
+console.log(exampleResult);
 
-// console.log(part1(data));
+console.log(part1(data));
 
-function part2(data: string[]) {
-    let result = data.map(x => x.split("\n").map(x => parseInt(x)))
-    let totals = result.flatMap(items => items.reduce((x,y) => x+y,0));
+function part2(data: number[][]) {
+    let totals = data.map(items => items.reduce((x,y) => x+y,0));
     totals.sort((a, b) => b - a);
     return totals[0] + totals[1] + totals[2];
 }
